eac3c79d96d790eab8b718123a4cf870cc84f346
Thiago Ribeiro <thiagorss54@gmail.com>
2020-05-30 22:01:20 -0300

    Adicionado informacao aleatoria sobre meu apm no starcraf2

--- Git-Teste/Program.cs
+++ Git-Teste/Program.cs
@@ -7,6 +7,7 @@ namespace Git_Teste
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Meu apm no starcraf2 é, em média, 166");
             Console.ReadLine();
 
         }
